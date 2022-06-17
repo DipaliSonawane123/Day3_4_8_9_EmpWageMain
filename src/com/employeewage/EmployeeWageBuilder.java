377184b442f5eefa29cca2f951ad38c793ba709a
Dipali <dipssonawane123@gmail.com>
2022-06-17 10:00:29 +0530

    Added: Employee Functionality added

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -1,7 +1,18 @@
 package com.employeewage;
 
+import java.util.Random;
+
 public class EmployeeWageBuilder {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Builder");
+        int IS_fULL_tIME = 1;
+        Random random = new Random();
+        int empCheck = random.nextInt(2);
+        if (empCheck == IS_fULL_tIME) {
+            System.out.println("Employee is present");
+        } else {
+            System.out.println("Employee is absent");
+
+        }
     }
 }
