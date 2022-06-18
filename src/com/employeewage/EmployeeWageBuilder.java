18d57c4808bedef56c58c1b09b6b9bd200427b4d
Dipali <dipssonawane123@gmail.com>
2022-06-18 05:35:41 +0530

    Added:Employee Functionality added

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -3,6 +3,7 @@ import java.util.Random;
 public class EmployeeWageBuilder {
 final   static  int FULL_TIME_WORKER=1;
   final static  int PART_TIME_WORKER=2;
+  final static int Total_Working_Day=20;
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Builder");
         int EMP_RATE_PER_HOUR=20;
@@ -28,6 +29,8 @@ final   static  int FULL_TIME_WORKER=1;
         }
 
         empWage=empHours*EMP_RATE_PER_HOUR;
+      int  Total_emp_wage=empWage*Total_Working_Day;
         System.out.println("The daily wage of Employee is" + empWage);
+        System.out.println("The Monthly Wage of Employee is"+Total_emp_wage);
     }
 }
