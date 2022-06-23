652417126efa622172fbd7d216a1dde9d7721297
Dipali <dipssonawane123@gmail.com>
2022-06-23 16:08:51 +0530

    Modified:Emp wage for multiple company modified

--- src/com/employeewage/EmployeeWageBuilder.java
+++ src/com/employeewage/EmployeeWageBuilder.java
@@ -1,55 +1,55 @@
-/*Refactor the Code to write a Class Method to Compute Employee Wage
-- Use Class Method and Class Variables*/
+/*Compute Employee Wage for multiple companies
+ - Note: Each Company has its own wage, number of working days and working hours per month
+- Use Class Method with function parameters instead of Class Variables*/
 package com.employeewage;
 import java.util.Random;
 public class EmployeeWageBuilder {
     final static int FULL_TIME_WORKER = 1;
     final static int PART_TIME_WORKER = 2;
-    final static int Total_Working_Day = 20;
-    final static int EMP_TOTAL_HOURS = 100;
-    final static int EMP_RATE_PER_HOUR = 20;
-public static void compempWage( ) {
-    int totalworkingdays = 0;
-    int empHours = 0;
-    int empWage = 0;
-    int Totalemphrs = 0;
-    System.out.println("Welcome to Employee Wage Builder");
 
-    while (Totalemphrs <= EMP_TOTAL_HOURS && totalworkingdays <= Total_Working_Day) {
-        totalworkingdays++;
-        Random random = new Random();
+    public static void computeEmpWage(String company, int EMP_RATE_PER_HOUR, int Total_no_Working_Day, int EMP_TOTAL_HOURS) {
+        int totalworkingdays = 0;
+        int empHours = 0;
+        int empWage = 0;
+        int Totalemphrs = 0;
+        System.out.println("Welcome to Employee Wage Builder");
 
-        int empCheck = random.nextInt(2);
+        while (Totalemphrs <= EMP_TOTAL_HOURS && totalworkingdays <= Total_no_Working_Day) {
+            totalworkingdays++;
+            Random random = new Random();
 
-        switch (empCheck) {
-            case FULL_TIME_WORKER:
-                empHours = 8;
-                System.out.println("Employee is present");
-                break;
-            case PART_TIME_WORKER:
-                empHours = 4;
-                System.out.println("Employee is present");
-                break;
-            default:
-                empHours = 0;
-                System.out.println("Employee is absent");
+            int empCheck = random.nextInt(2);
+
+            switch (empCheck) {
+                case FULL_TIME_WORKER:
+                    empHours = 8;
+                    System.out.println("Employee is present");
+                    break;
+                case PART_TIME_WORKER:
+                    empHours = 4;
+                    System.out.println("Employee is present");
+                    break;
+                default:
+                    empHours = 0;
+                    System.out.println("Employee is absent");
+            }
+            Totalemphrs = Totalemphrs + empHours;
+            empWage = empHours * EMP_RATE_PER_HOUR;
+            int Total_emp_wage = empWage + Totalemphrs;
+            Total_emp_wage = Total_emp_wage * Total_no_Working_Day;
+            System.out.println("The daily wage of Employee of " + company + " is" + empWage);
+            System.out.println("The Monthly Wage of Employee of " + company + " is" + Total_emp_wage);
+
         }
-        Totalemphrs = Totalemphrs + empHours;
-        empWage = empHours * EMP_RATE_PER_HOUR;
-        int Total_emp_wage = empWage + Totalemphrs;
-        Total_emp_wage = Total_emp_wage * Total_Working_Day;
-        System.out.println("The daily wage of Employee is" + empWage);
-        System.out.println("The Monthly Wage of Employee is" + Total_emp_wage);
+    }
+
+    public static void main(String[] args) {
+        computeEmpWage("Bridgelabzs", 20, 20, 200);
+        computeEmpWage("D Mart", 30, 30, 300);
+        computeEmpWage("HDFC Bank", 50, 20, 220);
 
     }
 }
-public static void main(String[] args) {
-        compempWage( );
-    }
-}
-
-
-
 
 
 
